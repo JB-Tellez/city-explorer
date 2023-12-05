fe0582d7ce735813f017dda2e3eb0b78629b023e
JB Tellez <jb.tellez@gmail.com>
2023-12-04 23:18:57 -0800

    adds data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import axios from 'axios';
 
-function App() {
-  const [count, setCount] = useState(0)
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+export default function App() {
+
+  const [location, setLocation] = useState({display_name:''});
+  const [searchQuery, setSearchQuery] = useState('');
+  const [mapUrl, setMapUrl] = useState();
+
+  async function fetchLocation() {
+    const url = `https://us1.locationiq.com/v1/search.php?key=${API_KEY}&q=${searchQuery}&format=json`;
+    const response = await axios.get(url);
+    const locationObj = response.data[0];
+    setLocation(locationObj);
+    setMapUrl(`https://maps.locationiq.com/v3/staticmap?key=${API_KEY}&center=${locationObj.lat},${locationObj.lon}&zoom=12`);
+  }
+
+  function updateQuery(event) {
+    setSearchQuery(event.target.value);
+  }
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input onChange={updateQuery} />
+      <button onClick={fetchLocation}>Explore!</button>
+      <h2>The city is: {location.display_name}</h2>
+      <img src={mapUrl} alt={location.display_name && `"Image of selected location ${location.display_name}"`} />
     </>
   )
 }
-
-export default App
